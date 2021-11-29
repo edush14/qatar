a49b2a07bba7d151ec5ebc8173222342f5b3a402
daveRQ <42696677+daveRQ@users.noreply.github.com>
2021-11-29 14:38:25 -0500

    website_res_partner

---
+++ website_res_partner/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'WEBSITE FIELDS',
+	'category': 'Website/Website',
+	'author': 'ITGRUPO',
+	'depends': ['base','website_partner','website_customer'],
+	'version': '1.0.0',
+	'description':"""
+	 Muestra campos que son visualizados en en el Sitio Web
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	['views/partner_it_view.xml'],
+	'installable': True
+}
