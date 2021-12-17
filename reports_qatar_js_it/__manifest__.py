158697223fd9c02bc738991ff7297bf6bcfa13c4
Jesus <jesus_sanchezjibaja@hotmail.com>
2021-12-17 11:48:18 -0500

    formato

---
+++ reports_qatar_js_it/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Reporte x Qatar',
+    'category': 'uncategorize',
+    'author': 'ITGRUPO',
+    'depends': ['sale_management'],
+    'version': '1.0',
+    'description':"""
+     Reporte x Qatar
+    """,
+    'auto_install': False,
+    'demo': [],
+    'data': [
+        #'security/security.xml',
+        'report/header.xml',
+        'report/sale_report.xml',
+        'report/sale_report_templates.xml',
+        ],
+    'installable': True
+}
